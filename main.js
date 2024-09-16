2092459d3ecdc67f06013f4c5138e1a5dbf40470
thelasthellcat-w <ysuarez_m21@outlook.com>
2024-09-16 18:48:07 -0500

    Validaciones, generar contrasena y boton limpiar
    Validar entrada de datos, generar contrasna con caracteres especiales, mensaje para determinar si la contrasna es fuerte o ebil y un boton para limpiar los inputs.

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 let cantidadInput = document.getElementById('cantidad');
 let generarBoton = document.getElementById('generar');
 let contrasenaInput = document.getElementById('contrasena');
-
-const caracteresOpciones = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+let limpiarBoton = document.getElementById('limpiar');
+let mensajeSpan = document.getElementById('mensaje');
+const caracteresOpciones = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
 
 function generarContrasena() {
 	let numeroDigitado = parseInt(cantidadInput.value);
-	if ( numeroDigitado<8 ) {
+	if ( numeroDigitado<8 || !numeroDigitado) {
 		alert("Cantidad de caracteres es inferior a lo requerido");
+		return;
 	}
 	let contrasena = '';
 	for (let i = 0; i < numeroDigitado; i++) {
@@ -15,5 +17,47 @@ function generarContrasena() {
 		let caracterAleatorio = caracteresOpciones[numeroAleatorio];
 		contrasena += caracterAleatorio;
 	}
+	validarContraseña(contrasena);
+}
+function validarContraseña(contrasena) {
+	let validarNumeros = contrasena.match(/[0-9]/g) ?? false;
+	let validarMayus = contrasena.match(/[A-Z]/g) ?? false;
+	let validarCaracteresEspeciales = contrasena.match(/[!@#$%^&*()]/g) ?? false;
+	
+	let mensajeContrasenaFuerte = "";
+	let mensajeContrasenaDebil = "Insegura: su contraseña es debil. ";
+	
+	if (validarNumeros==false) {
+		mensajeContrasenaDebil += "No tiene números. ";
+	}
+	if (validarMayus==false) {
+		mensajeContrasenaDebil += "No tiene mayúsculas. ";
+	}
+	if (validarCaracteresEspeciales==false) {
+		mensajeContrasenaDebil += "No tiene caracteres especiales.";
+	}
 	contrasenaInput.value = contrasena;
+	mensajeSpan.innerHTML = mensajeContrasenaDebil;
+	cambiarBotonLimpiar(false);
+
+	if ((contrasena.length >= 8) && (validarNumeros.length >= 1) && 
+		(validarMayus.length >= 1)&& (validarCaracteresEspeciales.length >= 1)) 
+	{
+		contrasenaInput.value = contrasena;
+		mensajeContrasenaFuerte += "Contraseña: Segura. Su contraseña es fuerte";
+		mensajeSpan.innerHTML = mensajeContrasenaFuerte;
+		cambiarBotonLimpiar(false);	
+		return;
+	}
+		
+}
+
+function limpiar() {
+	cantidadInput.value = '';
+	contrasenaInput.value = '';
+	mensajeSpan.innerHTML = '';
+	cambiarBotonLimpiar(true);
+}
+function cambiarBotonLimpiar(interruptor) {
+	limpiarBoton.hidden = interruptor;
 }
